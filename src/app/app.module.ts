576bb2ce6a1952cb0c3520f54db1df8be1f63b96
ruben <ruben.araus.g@gmail.com>
2017-09-20 17:58:51 -0500

    Commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { ClasesComponent } from './components/clases/clases.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
+import { APP_ROUTING } from "./app.routes";
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {UsuarioNuevoComponent} from "./components/usuario/usuario-nuevo.component";
+import {GraficaComponent} from "./components/grafica/grafica.component";
+import {Ng2WebpackComponent} from "./components/ng2-webpack/ng2-webpack.component";
+import {DndModule} from "ng2-dnd";
+import {Ng2SliderComponent} from "./components/ng2-slider/ng2-slider.component";
+import {FormsModule} from "@angular/forms";
+import {IonRangeSliderModule} from "ng2-ion-range-slider";
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgStyleComponent,
+    CssComponent,
+    ClasesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent,
+    GraficaComponent,
+    Ng2WebpackComponent,
+    Ng2SliderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    IonRangeSliderModule,
+    APP_ROUTING,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
